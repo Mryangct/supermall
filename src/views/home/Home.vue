c1bfc759c5f70deb24c3d3aa7e332909875d8105
yct <13692010355@139.com>
2020-05-31 00:36:53 +0800

    首页

---
+++ src/views/home/Home.vue
@@ -0,0 +1,20 @@
+<template>
+ <div id="home">
+   <nar-bar></nar-bar>
+ </div>
+</template>
+
+<script>
+import NavBar from 'components/common/navbar/NarBar'
+export default {
+  name: "Home",
+  components: {
+    NavBar
+  }
+
+};
+</script>
+
+<style scoped>
+
+</style>
